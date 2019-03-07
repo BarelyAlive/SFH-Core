76c71135489b9349c8f87bc9941b966a0866f8f1
Angayoco <angayoco@protonmail.com>
2019-03-07 13:03:21 +0100

    Tile Entities
    Trying to pdate Tile Entities to 1.12.2 and to make extendable Base TE classes for SFHCore

--- main/java/mod/sfhcore/tileentities/TileEntityBase.java
+++ main/java/mod/sfhcore/tileentities/TileEntityBase.java
@@ -5,11 +5,14 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
@@ -20,27 +23,23 @@ import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.FluidTankInfo;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidTankProperties;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import scala.Int;
 
-public class TileEntityBase extends TileEntity implements IInventory{
+public class TileEntityBase extends TileEntity implements ITickable,IInventory{
 	
-	private ItemStack[] inv;
+	private NonNullList<ItemStack> machineItemStacks = NonNullList.<ItemStack>withSize(3, ItemStack.EMPTY);
 	private String field_145958_o;
 	public static final int maxworkTime = 100;
 	public int workTime;
 	public static final int MAX_CAPACITY = 16000;
 	float volume;
-	public FluidStack fluid;
-	public FluidTank tank = new FluidTank(fluid, (int) volume);
-	World world;
-	Block bl;
-	EntityPlayer player;
+	World world = this.getWorld();
 	
 	public TileEntityBase() {
-		inv = new ItemStack[2];
 		workTime = 0;
 		volume = 0;
-		fluid = new FluidStack(FluidRegistry.WATER, 0);
 	}
 	
 	/**
@@ -53,75 +52,88 @@ public class TileEntityBase extends TileEntity implements IInventory{
 	
 	public void update() {
 	}
-	
-	private boolean canFreeze()
-    {
-        if (this.fluid.amount < 1000)
-        {
-            return false;
-        }
-        else
-        {
-            ItemStack itemstack = new ItemStack(Blocks.ICE);
-            if (this.inv[0] == null) return true;
-            if (!this.inv[0].isItemEqual(itemstack)) return false;
-            int result = inv[0].getCount() + itemstack.getCount();
-            return result <= getInventoryStackLimit() && result <= this.inv[0].getMaxStackSize(); //Forge BugFix: Make it respect stack sizes properly.
-        }
-    }
-
-    public void freezeItem()
-    {
-        if (this.canFreeze())
-        {
-        	ItemStack itemstack = new ItemStack(Blocks.ICE);
-
-            if (this.inv[0] == null)
-            {
-                this.inv[0] = itemstack.copy();
-            }
-            else if (this.inv[0].getItem() == itemstack.getItem())
-            {
-                this.inv[0].setCount(this.inv[0].getCount() + itemstack.getCount());; // Forge BugFix: Results may have multiple items
-            }
-        }
-        
-    }
     
+	/**
+     * Returns the number of slots in the inventory.
+     */
 	@Override
-	public int getSizeInventory() {
-		return inv.length;
-	}
+    public int getSizeInventory()
+    {
+        return this.machineItemStacks.size();
+    }
 	
-	@Override
-	public ItemStack getStackInSlot(int slot) {
-		return inv[slot];
-	}
-	
-	@Override
-	public ItemStack decrStackSize(int slot, int amt) {
-		ItemStack stack = getStackInSlot(slot);
-		if(stack != null) {
-			if(stack.getCount() <= amt) {
-				setInventorySlotContents(slot, null);
-			}
-			else {
-				stack = stack.splitStack(amt);
-				if(stack.getCount() == 0) {
-					setInventorySlotContents(slot, null);
-				}
-			}
-		}
-		return stack;
-	}
+    @Override
+    public boolean isEmpty()
+    {
+        for (ItemStack itemstack : this.machineItemStacks)
+        {
+            if (!itemstack.isEmpty())
+            {
+                return false;
+            }
+        }
 
-	@Override
-	public void setInventorySlotContents(int slot, ItemStack stack) {
-		inv[slot] = stack;
-		if(stack != null && stack.getCount() >= getInventoryStackLimit()) {
-			stack.setCount(getInventoryStackLimit());
-		}
-	}
+        return true;
+    }
+
+    /**
+     * Removes up to a specified number of items from an inventory slot and returns them in a new stack.
+     */
+    public ItemStack decrStackSize(int index, int count)
+    {
+        return ItemStackHelper.getAndSplit(this.machineItemStacks, index, count);
+    }
+
+    /**
+     * Removes a stack from the given slot and returns it.
+     */
+    public ItemStack removeStackFromSlot(int index)
+    {
+        return ItemStackHelper.getAndRemove(this.machineItemStacks, index);
+    }
+	
+    /**
+     * Sets the given item stack to the specified slot in the inventory (can be crafting or armor sections).
+     */
+    public void setInventorySlotContents(int index, ItemStack stack)
+    {
+        ItemStack itemstack = this.machineItemStacks.get(index);
+        boolean flag = !stack.isEmpty() && stack.isItemEqual(itemstack) && ItemStack.areItemStackTagsEqual(stack, itemstack);
+        this.machineItemStacks.set(index, stack);
+
+        if (stack.getCount() > this.getInventoryStackLimit())
+        {
+            stack.setCount(this.getInventoryStackLimit());
+        }
+
+        if (index == 0 && !flag)
+        {
+            this.maxWorkTime = this.getWorkTime(stack);
+            this.workTime = 0;
+            this.markDirty();
+        }
+    }
+
+    /**
+     * Get the name of this object. For players this returns their username
+     */
+    public String getName()
+    {
+        return this.hasCustomName() ? this.machineCustomName : "container.machine";
+    }
+
+    /**
+     * Returns true if this thing is named
+     */
+    public boolean hasCustomName()
+    {
+        return this.machineCustomName != null && !this.machineCustomName.isEmpty();
+    }
+
+    public void setCustomInventoryName(String p_145951_1_)
+    {
+        this.machineCustomName = p_145951_1_;
+    }
 
 	@Override
 	public int getInventoryStackLimit() {
@@ -146,7 +158,6 @@ public class TileEntityBase extends TileEntity implements IInventory{
 			}
 		}
 		this.workTime = nbt.getShort("workTime");
-		this.fluid.amount = nbt.getShort("fluid");
 	}
 	
 	@Override
@@ -168,103 +179,14 @@ public class TileEntityBase extends TileEntity implements IInventory{
 		return nbt;
 	}
 
-	@Override
-	public boolean isItemValidForSlot(int slot, ItemStack itemstack) {
-		return false;
-	}
-
 	public int getWorkTimeRemainingScaled(int i) {
 		return this.workTime * i / this.maxworkTime;
 	}
-	
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public boolean hasCustomName() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public ITextComponent getDisplayName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public int fill(FluidStack resource, boolean doFill) {
-		return 0;
-	}
-
-	@Override
-	public FluidStack drain(FluidStack resource, boolean doDrain) {
-		if(resource.getFluid() == FluidRegistry.WATER)
-		return new FluidStack(FluidRegistry.WATER, Int.MaxValue());
-		return null;
-	}
-
-	@Override
-	public FluidStack drain(int maxDrain, boolean doDrain) {
-		// TODO Auto-generated method stub
-		return new FluidStack(FluidRegistry.WATER, Int.MaxValue());
-	}
-
-	@Override
-	public IFluidTankProperties[] getTankProperties() {
-		IFluidTankProperties[] prop = new IFluidTankProperties[fluid.amount];
-		return prop;
-		}
-
-	@Override
-	public ItemStack removeStackFromSlot(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void openInventory(EntityPlayer player) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void closeInventory(EntityPlayer player) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getField(int id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setField(int id, int value) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getFieldCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @SideOnly(Side.CLIENT)
+    public static boolean isWorking(IInventory inventory)
+    {
+        return inventory.getField(0) > 0;
+    }
 	
 }
