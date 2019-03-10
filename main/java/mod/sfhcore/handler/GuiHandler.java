c34569c0a0e4948132fed774fd52b2e1a297d4d4
Gaprosys <gaprosys@gmail.com>
2019-03-10 16:42:52 +0100

    Alle Objekte in Classen umgewandelt

--- main/java/mod/sfhcore/handler/GuiHandler.java
+++ main/java/mod/sfhcore/handler/GuiHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
+import mod.sfhcore.blocks.Cube;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
@@ -18,8 +19,8 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public class GuiHandler implements IGuiHandler {
-
-	protected static List<Pair<TileEntity, Container>> teco = new ArrayList<Pair<TileEntity, Container>>();
+	
+	protected static List<Pair<Class<TileEntity>, Class<Container>>> teco = new ArrayList<Pair<Class<TileEntity>, Class<Container>>>();
 	
 	public GuiHandler(Object mod) {
 		NetworkRegistry.INSTANCE.registerGuiHandler(mod, this);
@@ -29,10 +30,10 @@ public class GuiHandler implements IGuiHandler {
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		TileEntity te1 = world.getTileEntity(new BlockPos(x, y, z));
 		
-			for(Pair<TileEntity, Container> tc : teco) {
+			for(Pair<Class<TileEntity>, Class<Container>> tc : teco) {
 				if(te1.getClass().equals(tc.getLeft().getClass())){
 					
-					Class<Container> c = (Class<Container>) tc.getRight().getClass();
+					Class<Container> c = (Class<Container>) tc.getRight();
 					try {
 					Constructor<Container> con = c.getConstructor((Class<InventoryPlayer>) player.inventory.getClass(), (Class<TileEntity>) te1.getClass());
 					return con.newInstance(player.inventory, te1);
@@ -50,10 +51,10 @@ public class GuiHandler implements IGuiHandler {
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		TileEntity te1 = world.getTileEntity(new BlockPos(x, y, z));
 		
-		for(Pair<TileEntity, Container> tc : teco) {
+		for(Pair<Class<TileEntity>, Class<Container>> tc : teco) {
 			if(te1.getClass().equals(tc.getLeft().getClass())){
 				
-				Class<Container> c = (Class<Container>) tc.getRight().getClass();
+				Class<Container> c = (Class<Container>) tc.getRight();
 				try {
 				Constructor<Container> con = c.getConstructor((Class<InventoryPlayer>) player.inventory.getClass(), (Class<TileEntity>) te1.getClass());
 				return con.newInstance(player.inventory, te1);
@@ -73,8 +74,8 @@ public class GuiHandler implements IGuiHandler {
 	 * @param time
 	 * @return
 	 */
-	public static boolean addGUIRelation(TileEntity tile, Container con) {
-		return teco.add(new ImmutablePair<TileEntity, Container>(tile, con));
+	public static boolean addGUIRelation(Object tile, Object con) {
+		return teco.add(new ImmutablePair<Class<TileEntity>, Class<Container>>((Class<TileEntity>)tile, (Class<Container>)con));
 	}
 }
 
