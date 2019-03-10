f1e68ab4404c1e20f0aa85fb37f6def125a86fcc
Gaprosys <gaprosys@gmail.com>
2019-03-10 12:42:27 +0100

    RegistrationHandler von einer Datei in die Core-Klasse verschoben

--- main/java/mod/sfhcore/SFHCore.java
+++ main/java/mod/sfhcore/SFHCore.java
@@ -1,12 +1,17 @@
 package mod.sfhcore;
  
+import mod.sfhcore.handler.RegisterBlocks;
+import mod.sfhcore.handler.RegisterItems;
 import mod.sfhcore.proxy.SFHCoreClientProxy;
 import mod.sfhcore.proxy.SFHCoreProxy;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.world.WorldType;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.terraingen.WorldTypeEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
@@ -16,6 +21,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -29,6 +35,30 @@ public class SFHCore {
     @SidedProxy(clientSide="mod.sfhcore.proxy.SFHCoreClientProxy", serverSide="mod.sfhcore.proxy.SFHCoreProxy")
     public static SFHCoreProxy proxy;
      
+    @Mod.EventBusSubscriber
+    public static class RegistrationHandler {
+    	
+    	@SubscribeEvent
+    	public static void registerBlocks (RegistryEvent.Register<Block> event)
+    	{
+    		RegisterBlocks.register(event.getRegistry());
+    	}
+    	
+    	@SubscribeEvent
+    	public static void registerItems (RegistryEvent.Register<Item> event)
+    	{
+    		RegisterItems.register(event.getRegistry());
+    		RegisterBlocks.registerItemBlocks(event.getRegistry());
+    	}
+    	
+    	public static void registerModels(ModelRegistryEvent event)
+    	{
+    		RegisterItems.registerModels();
+    		RegisterBlocks.registerModels();
+    	}
+
+    }
+
          
     @EventHandler
     public void PreInit(FMLPreInitializationEvent event){
