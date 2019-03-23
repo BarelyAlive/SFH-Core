1144f260d7809ce172ade644cc9f8e5f32918167
Angayoco <angayoco@protonmail.com>
2019-03-23 22:09:20 +0100

    Update SFHCoreClientProxy.java

--- main/java/mod/sfhcore/proxy/SFHCoreClientProxy.java
+++ main/java/mod/sfhcore/proxy/SFHCoreClientProxy.java
@@ -43,7 +43,7 @@ public class SFHCoreClientProxy extends SFHCoreProxy{
         {
             IVariantProvider variantProvider = (IVariantProvider) item;
             for (Pair<Integer, String> variant : variantProvider.getVariants())
-                ModelLoader.setCustomModelResourceLocation(item, variant.getLeft(), new ModelResourceLocation(modid + ":" + name, "inventory")); //new ResourceLocation(modid, name), variant.getRight()));
+                ModelLoader.setCustomModelResourceLocation(item, variant.getLeft(), new ModelResourceLocation(new ResourceLocation(modid, name), variant.getRight())); //new ResourceLocation(modid, name), variant.getRight()));
         }
         else
         {
