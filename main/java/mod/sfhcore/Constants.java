9b670d08fe386b1287d8bec07e8149acec31704d
Angayoco <angayoco@protonmail.com>
2019-03-23 20:23:10 +0100

    Update Constants.java

--- main/java/mod/sfhcore/Constants.java
+++ main/java/mod/sfhcore/Constants.java
@@ -2,14 +2,11 @@ package mod.sfhcore;
 
 public class Constants {
 
-	public static final String ModIdSFHCORE = "sfhcore";
 	public static final String SFHCoreVersion = "1.1.0";
 	
-	public static final String ModIdNTM = "nethertweaks";
 	public static final String NTMVersion = "1.7.0";
     public static final String MODCORE = "after:nethertweaksmod";
 	
-	public static final String ModIdCHAUST = "chaust";
 	public static final String ChaustVersion = "0.0.4";
 	
 	public static final String MOD = "nethertweaksmod";
