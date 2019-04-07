d01d8c0b46a8518dce79be4acf91167b8a1dc8a1
Angayoco <angayoco@protonmail.com>
2019-04-07 18:35:45 +0200

    asdf

---
+++ main/java/mod/sfhcore/util/ItemInfo.java
@@ -0,0 +1,158 @@
+package mod.sfhcore.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+
+public class ItemInfo
+{
+    public ItemInfo(Item item, int meta)
+    {
+    	this.item = item;
+    	this.meta = meta;
+    }
+	
+    private Item item;
+    
+    public Item getItem()
+    {
+    	return item;
+    }
+    
+    public Item setItem(int meta)
+    {
+    	return item;
+    }
+
+    private int meta;
+    
+    public int getMeta()
+    {
+    	return meta;
+    }
+    
+    public int setMeta(int count)
+    {
+    	return (meta - count);
+    }
+    
+    public static ItemInfo getItemInfoFromStack(ItemStack stack)
+    {
+        return new ItemInfo(stack);
+    }
+    
+    public ItemInfo(ItemStack stack)
+    {
+        item = stack == null ? null : stack.getItem();
+        meta = stack == null ? -1 : stack.getMetadata();
+    }
+    
+    public ItemInfo(Block block, int blockMeta)
+    {
+        item = Item.getItemFromBlock(block);
+        meta = block == null ? -1 : blockMeta;
+    }
+    
+    public ItemInfo(String string)
+    {
+        String[] split = string.split(":");
+        
+        if(split.length == 1)
+        {
+            item = Item.getByNameOrId("minecraft:" + split[0]);
+        }
+        else if(split.length == 2)
+        {
+            try
+            {
+                meta = split[1].equals("*") ? -1 : Integer.parseInt(split[1]);
+                item = Item.getByNameOrId("minecraft:" + split[0]);
+            }
+            catch(NumberFormatException e)
+            {
+                meta = -1;
+                item = Item.getByNameOrId(split[0] + ":" + split[1]);
+            }
+        }
+        else if(split.length == 3)
+        {
+            try
+            {
+                meta = split[2].equals("*") ? -1 : Integer.parseInt(split[2]);
+                item = Item.getByNameOrId(split[0] + ":" + split[1]);
+            }
+            catch(NumberFormatException e)
+            {
+                meta = -1;
+            }
+        }
+        else
+        {
+            meta = -1;
+        }
+    }
+    
+    public ItemInfo(IBlockState state)
+    {
+        item = state == null ? null : Item.getItemFromBlock(state.getBlock());
+        meta = state == null ? -1 : state.getBlock().getMetaFromState(state);
+    }
+    
+    public String toString()
+    {
+        return Item.REGISTRY.getNameForObject(item) + (meta == -1 ? "" : (":" + meta));
+    }
+    
+    public ItemStack getItemStack()
+    {
+        return item == null ? null : new ItemStack(item, 1, meta == -1 ? 0 : meta);
+    }
+    
+    public NBTTagCompound writeToNBT(NBTTagCompound tag)
+    {
+        tag.setString("item", Item.REGISTRY.getNameForObject(item).toString());
+        tag.setInteger("meta", meta);
+        
+        return tag;
+    }
+    
+    public static ItemInfo readFromNBT(NBTTagCompound tag)
+    {
+        Item item_ = Item.REGISTRY.getObject(new ResourceLocation(tag.getString("item")));
+        int meta_ = tag.getInteger("meta");
+        
+        return new ItemInfo(item_, meta_);
+    }
+    
+    public int hashCode()
+    {
+        return item == null ? 41 : item.hashCode();
+    }
+    
+    public boolean equals(Object other)
+    {
+        if (other instanceof ItemInfo)
+        {
+            ItemInfo info = (ItemInfo) other;
+            
+            if(item == null || info.item == null)
+            {
+                return false;
+            }
+            
+            if (meta == -1 || info.meta == -1)
+            {
+                return item.equals(info.item);
+            }
+            else
+            {
+                return  meta == info.meta && item.equals(info.item);
+            }
+        }
+        
+        return false;
+    }
+}
