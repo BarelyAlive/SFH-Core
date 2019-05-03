e0140773ea5e0e1a96a2be163c1c7c8f949625cb
Angayoco <angayoco@protonmail.com>
2019-05-03 20:28:46 +0200

    Moved useful render stuff from ntm to SFHCore

---
+++ main/java/mod/sfhcore/client/renderers/SpriteColor.java
@@ -0,0 +1,26 @@
+package mod.sfhcore.client.renderers;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import mod.sfhcore.texturing.*;
+
+public class SpriteColor {
+    private TextureAtlasSprite sprite;
+    private mod.sfhcore.texturing.Color color;
+    
+    public SpriteColor(TextureAtlasSprite sprite, mod.sfhcore.texturing.Color color2)
+    {
+    	this.sprite = sprite;
+    	this.color = color2;
+    }
+    
+    public TextureAtlasSprite getSprite()
+    {
+    	return this.sprite;
+    }
+    
+    public mod.sfhcore.texturing.Color getColor()
+    {
+    	return this.color;
+    }
+    
+}
