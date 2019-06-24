bdda0346293a2be1ae1178775cf0d8865c490ab0
Angayoco <angayoco@protonmail.com>
2019-06-24 11:16:29 +0200

    Create PlayerInventory.java

---
+++ main/java/mod/sfhcore/helper/PlayerInventory.java
@@ -0,0 +1,21 @@
+package mod.sfhcore.helper;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+public class PlayerInventory
+{
+	public static boolean tryAddItem(EntityPlayer player, ItemStack stack)
+	{
+		if (player != null) {
+			if (!player.inventory.addItemStackToInventory(stack.copy()) && !player.world.isRemote) {
+				EntityItem item = new EntityItem(player.world, player.getPosition().getX(), player.getPosition().getY(),
+						player.getPosition().getZ(), stack.copy());
+				player.world.spawnEntity(item);
+				return false;
+			} 
+		}
+		return true;
+	}
+}
