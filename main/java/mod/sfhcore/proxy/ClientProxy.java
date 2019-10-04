e05b3d1749fe213dd9627426c51abe124bb1542e
Angayoco <angayoco@protonmail.com>
2019-10-04 18:40:23 +0200

    asdf

--- main/java/mod/sfhcore/proxy/ClientProxy.java
+++ main/java/mod/sfhcore/proxy/ClientProxy.java
@@ -2,12 +2,16 @@ package mod.sfhcore.proxy;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import mod.sfhcore.Constants;
+import mod.sfhcore.helper.FluidStateMapper;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -55,5 +59,20 @@ public class ClientProxy extends CommonProxy{
 				ModelLoader.setCustomModelResourceLocation(item, variant.getLeft(), new ModelResourceLocation(loc, variant.getRight()));
 		} else if (!item.getHasSubtypes())
 			ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(loc, INVENTORY));
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void initModel(final Fluid f, final Block b)
+	{
+		FluidStateMapper mapper = new FluidStateMapper(b.getRegistryName().getResourceDomain(), f);
+
+		Item item = Item.getItemFromBlock(b);
+		if (item != null) {
+			ModelBakery.registerItemVariants(item);
+			ModelLoader.setCustomMeshDefinition(item, mapper);
+		}
+
+		ModelLoader.setCustomStateMapper(b, mapper);
 	}
 }
--- main/java/mod/sfhcore/proxy/CommonProxy.java
+++ main/java/mod/sfhcore/proxy/CommonProxy.java
@@ -4,22 +4,15 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
 
 public class CommonProxy {
 
-	public void tryHandleBlockModel(final Block block, final ResourceLocation loc)
-	{
-		// NO-OP
-	}
+	public void tryHandleBlockModel(final Block block, final ResourceLocation loc) {}
 
-	public void tryHandleItemModel(final Item item, final ResourceLocation loc)
-	{
-		// NO-OP
-	}
+	public void tryHandleItemModel(final Item item, final ResourceLocation loc) {}
 
-	public void tryHandleBlockModel(final ItemBlock block, final ResourceLocation loc) {
-		// TODO Auto-generated method stub
+	public void tryHandleBlockModel(final ItemBlock block, final ResourceLocation loc) {}
 
-	}
-
+	public void initModel(final Fluid f, final Block b) {}
 }
