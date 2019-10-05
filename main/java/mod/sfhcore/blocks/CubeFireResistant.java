ca02bb4a7e98d4993e5c3eaa382f6a58b460db3e
Angayoco <angayoco@protonmail.com>
2019-10-05 16:23:12 +0200

    Create CubeFireResistant.java

---
+++ main/java/mod/sfhcore/blocks/CubeFireResistant.java
@@ -0,0 +1,21 @@
+package mod.sfhcore.blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class CubeFireResistant extends Cube{
+
+	public CubeFireResistant(Material material, float resistance, float hardness, CreativeTabs tab,
+			ResourceLocation loc) {
+		super(material, resistance, hardness, tab, loc);
+	}
+
+	@Override
+	public boolean isFlammable(final IBlockAccess world, final BlockPos pos, final EnumFacing face) {
+		return false;
+	}
+}
