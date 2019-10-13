4b60d5fb6283109077026540ac33feb2d67a1c41
Angayoco <angayoco@protonmail.com>
2019-10-13 15:09:06 +0200

    Update NetworkHandler.java

--- main/java/mod/sfhcore/network/NetworkHandler.java
+++ main/java/mod/sfhcore/network/NetworkHandler.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class NetworkHandler
 {
 	public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(Constants.MODID);
-	private static int id = 0;
+	private static int id = 1;
 
 	public static void registerMessage(final Class messageHandler, final Class message, final Side side)
 	{
